5240e059b15a94d82f9679aa75dd5a1b4d2aee65
Mijeong Ban <mban1@stevens.edu>
2019-11-14 10:54:34 -0500

    Query and Mutation setup

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,21 @@
+const addProject = async (parent, args) => {};
+const updateProject = async (parent, args) => {};
+const deleteProject = async (parent, args) => {};
+const addTask = async (parent, args) => {};
+const updateTask = async (parent, args) => {};
+const deleteTask = async (parent, args) => {};
+const addColumn = async (parent, args) => {};
+const updateColumn = async (parent, args) => {};
+const deleteColumn = async (parent, args) => {};
+
+module.exports = {
+  addProject,
+  updateProject,
+  deleteProject,
+  addTask,
+  updateTask,
+  deleteTask,
+  addColumn,
+  updateColumn,
+  deleteColumn
+};
---
+++ src/resolvers/Query.js
@@ -0,0 +1,17 @@
+const projects = async (parent, args) => {};
+
+const project = async (parent, args) => {};
+
+const tasks = async (parent, args) => {};
+
+const task = async (parent, args) => {};
+
+const user = async (parent, args) => {};
+
+module.exports = {
+  projects,
+  project,
+  tasks,
+  task,
+  user
+};
