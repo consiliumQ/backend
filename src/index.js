41afdc8ae490a5fd5a9080f5437dc8a3592734ee
Mijeong Ban <mban1@stevens.edu>
2019-11-14 10:21:08 -0500

    initial setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const { ApolloServer, gql } = requiire("apollo-server");
+
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+
+const resolvers = {
+  Query,
+  Mutation
+};
+
+const server = new ApolloServer({
+  typeDefs: "./schema.graphql",
+  resolvers
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url} 🚀`);
+});
